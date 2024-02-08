8c0a2979fcbaee66c6f0045f1889cee991e553f4
Chance Murray <chance.s.murray@gmail.com>
2024-02-07 19:54:44 -0700

    infrastructure and directory setup

---
+++ scheduler-service/main.go
@@ -0,0 +1,20 @@
+package main
+
+// This is a placeholder program, unrelated to the training course. You
+// are not expected to run or modify this file. Having a Go program in
+// the project root avoids warning messages shown when running "go get"
+// from this directory. See: https://github.com/golang/go/issues/37700
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	fmt.Println(string("scheduler-service"))
+
+	// Respond to web requests
+
+	// Also Read from kafka
+}
